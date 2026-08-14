ecfbda0c385bb88b57fa31f835949784b22333ba
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-14 16:28:04 -0300

    Atualização de portifólio

---
+++ studies/aula02/ex02.js
@@ -0,0 +1,12 @@
+let nome = "Guilherme"
+let idade = 22;
+let altura = 1.75;
+let estudando = true;
+
+console.log(nome, typeof nome,
+            idade, typeof idade,
+            altura, typeof altura,
+            estudando, typeof estudando);
+console.log(typeof idade);
+console.log(typeof altura);
+console.log(typeof estudando);
