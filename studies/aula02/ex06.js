e149b34ae7c543c36d237e046237cd10f6b816d3
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-14 16:51:01 -0300

    Atualização de portifólio

--- studies/aula02/ex06.js
+++ studies/aula02/ex06.js
@@ -1,6 +1,6 @@
-numero1 = "10";
-numero2 = 10;
+let numero1 = "10";
+let numero2 = 10;
 
-variavel = numero1 + numero2;
+let variavel = numero1 + numero2;
 
 console.log(variavel);
---
+++ studies/aula02/ex07.js
@@ -0,0 +1,4 @@
+let valor = "20";
+let resultado = valor * 2;
+
+console.log(resultado);
