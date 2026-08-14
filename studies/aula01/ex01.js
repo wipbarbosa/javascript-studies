ba5a60bb39e40932c25e83b20a09c3e44f1e7e88
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-14 15:50:58 -0300

    Atualização de portifólio

--- studies/aula01/ex01.js
+++ studies/aula01/ex01.js
@@ -1,1 +1,3 @@
-console.log("Olá, mundo!");
+let nome = 'guilhermme';
+
+console.log(nome);
---
+++ studies/aula01/ex02.js
@@ -0,0 +1,10 @@
+let numero1 = 10;
+let numero2 = 2;
+
+soma = numero1 + numero2;
+subtracao = numero1 - numero2;
+multiplicacao =  numero1 * numero2;
+divisao = numero1 % numero2;
+
+
+console.log(soma, subtracao, multiplicacao, divisao);
