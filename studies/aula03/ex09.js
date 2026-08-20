7216fa74ace9d2cbf28089ffca54c1bfffa23108
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-20 16:27:15 -0300

    Atualização de portifólio

---
+++ studies/aula03/ex09.js
@@ -0,0 +1,10 @@
+let idade = 20;
+let nota = 5;
+
+if (idade >= 18 && nota >= 7) {
+    console.log("Aprovado");
+} else if (idade >= 18 && nota >= 5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
