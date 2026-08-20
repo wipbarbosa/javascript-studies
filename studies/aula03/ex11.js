d5f3128edace08d829e48b868da165cf02b6a875
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-20 16:42:49 -0300

    Atualização de portifólio

---
+++ studies/aula03/ex11.js
@@ -0,0 +1,9 @@
+let idade = 20;
+let temCarteira = true;
+let bloqueado = false;
+
+if (idade >= 18 && (temCarteira && !bloqueado)){
+    console.log("Pode dirigir")
+} else{
+    ("Não pode dirigir")
+}
