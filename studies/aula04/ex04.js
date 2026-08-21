b747061d97d8db656ac1727fdc6f22702c41d42c
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-21 15:00:07 -0300

    Atualização de portifólio

--- studies/aula04/ex04.js
+++ studies/aula04/ex04.js
@@ -1,5 +1,5 @@
-let contador = 0
-let soma = 0
+let contador = 0;
+let soma = 0;
 
 while(contador <= 5){
     soma += contador;
---
+++ studies/aula04/ex07.js
@@ -0,0 +1,11 @@
+let contador = 0;
+
+while (contador <= 30) {
+    if (contador % 2 == 0) {
+        console.log("PAR: ", contador);
+    } else {
+        console.log("ÍMPAR: ", contador);
+    }
+
+    contador++;
+}
