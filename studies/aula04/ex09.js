f08558f67e7032cad80898142949b73bf285062d
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-26 14:30:21 -0300

    Atualização de portifólio

--- studies/aula04/ex09.js
+++ studies/aula04/ex09.js
@@ -3,6 +3,12 @@ let pares = 0
 let impares = 0
 
 while (contador <= 30){
-    console.log(contador)
+    if(contador % 2 == 0){
+        pares++
+    }
+    if(contador % 2 == 1){
+        impares++
+    }
     contador++
 }
+console.log("Pares: ", pares, "Impares: ", impares)
